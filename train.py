a109c81d583f1bfad0e35894c73f3c5dd0602bbc
rnlee1998 <1050145004@qq.com>
2021-09-06 17:32:57 +0800

    修改

--- train.py
+++ train.py
@@ -421,7 +421,7 @@ def parse_opt(known=False):
     parser = argparse.ArgumentParser()
     parser.add_argument('--weights', type=str, default='yolov5s.pt', help='initial weights path')
     parser.add_argument('--cfg', type=str, default='', help='model.yaml path')
-    parser.add_argument('--data', type=str, default='data/mydata.yaml', help='dataset.yaml path')
+    parser.add_argument('--data', type=str, default='data\coco128.yaml', help='dataset.yaml path')
     parser.add_argument('--hyp', type=str, default='data/hyps/hyp.scratch.yaml', help='hyperparameters path')
     parser.add_argument('--epochs', type=int, default=10)
     parser.add_argument('--batch-size', type=int, default=12, help='total batch size for all GPUs')
