21dda5fc95b4beb70b1f6cf2c3d819e339505541
rnlee1998 <1050145004@qq.com>
2021-09-07 11:10:59 +0800

    FAM

--- new.py
+++ new.py
@@ -1,6 +1,7 @@
 # 这是一个测试文件，与yolo无关，只是用来 测试一部分函数功能
 
 import argparse
+import sys
 
 from torch import nn
 from torch._C import Size
@@ -18,23 +19,27 @@ import torchvision
 from torchvision import transforms
 from thop import profile
 
+from pathlib import Path
 
-# model = Model()
-# input = torch.randn(1, 3, 224, 224)
-# flops, params = profile(model, inputs=(input, ))
-# print('flops:', flops)
+FILE = Path(__file__).absolute()
+sys.path.append(FILE.parents[1].as_posix())  # add yolov5/ to path
 
+cfg='models/yolov5m_mydata.yaml'
+import yaml  # for torch hub
+yaml_file = Path(cfg).name
+with open(cfg) as f:
+    yaml = yaml.safe_load(f)  # model dict
+print(yaml)
+# parser = argparse.ArgumentParser()
+# parser.add_argument('--cfg', type=str, default='models/yolov5m_mydata.yaml', help='model.yaml')
+# parser.add_argument('--device', default='0', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+# opt = parser.parse_args()
+# opt.cfg = check_file(opt.cfg) 
+# device = select_device(opt.device)
+# print(opt.cfg)
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--cfg', type=str, default='models/yolov5m_mydata.yaml', help='model.yaml')
-parser.add_argument('--device', default='0', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
-opt = parser.parse_args()
-opt.cfg = check_file(opt.cfg) 
-device = select_device(opt.device)
-print(opt.cfg)
-
-model = Model(opt.cfg).to(device)
-print(model)
+# model = Model(opt.cfg).to(device)
+# print(model)
 
 # a = torch.randn(size=(4, 3, 128, 128),device=torch.device("cuda"))
 # y = trans(x)
